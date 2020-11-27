81ec93e05ec527c956213db489d54381952b1b95
Hovhannes Babayan <hovhannesbabayan@workfront.com>
2020-11-27 21:11:11 +0400

    refactor: switch to jest and github actions

---
+++ src/loader.js
@@ -0,0 +1,98 @@
+const loaderUtils = require("loader-utils");
+
+const { DOMParser, XMLSerializer } = require("xmldom");
+const xpath = require("xpath");
+const crypto = require("crypto");
+
+module.exports = function (content) {
+  this.cacheable && this.cacheable();
+
+  const query = loaderUtils.getOptions(this);
+  const config = {
+    tag: "symbol",
+  };
+  let context;
+
+  Object.keys(query).forEach(function (attr) {
+    config[attr] = query[attr];
+  });
+
+  context = config.context;
+  if (!context && this.options) {
+    context = this.options.context;
+  }
+  content = content.toString("utf8");
+
+  const targetDoc = new DOMParser().parseFromString(
+    "<" + config.tag + "></" + config.tag + ">",
+    "text/xml"
+  );
+  const targetEl = targetDoc.documentElement;
+
+  const svgDoc = new DOMParser().parseFromString(content, "text/xml");
+  const svgEl = svgDoc.documentElement;
+
+  // Transfer supported attributes from SVG element to the target element.
+  // Attributes provided via loader query string overwrite attributes set to SVG element.
+  const attributes = ["viewBox", "height", "width", "preserveAspectRatio"];
+  attributes.forEach(function (attr) {
+    if (query[attr]) {
+      targetEl.setAttribute(attr, query[attr]);
+    } else if (svgEl.hasAttribute(attr)) {
+      targetEl.setAttribute(attr, svgEl.getAttribute(attr));
+    }
+  });
+
+  // Apply additional attributes provided via loader query string
+  ["id", "class"].forEach(function (param) {
+    if (query[param]) {
+      const interpolateOptions = {
+        content: content,
+        regExp: config.regExp,
+      };
+      if (context) {
+        interpolateOptions.context = context;
+      }
+      targetEl.setAttribute(
+        param,
+        loaderUtils.interpolateName(this, query[param], interpolateOptions)
+      );
+    }
+  }, this);
+
+  // Move all child nodes from SVG element to the target element
+  let el = svgEl.firstChild;
+  while (el) {
+    targetEl.appendChild(targetDoc.importNode(el, true));
+    el = el.nextSibling;
+  }
+
+  // Append unique prefix to all used 'id' attributes to make them unique in the universe
+  const nodesWithId = xpath.select("/*/*[@id]", targetDoc);
+  if (nodesWithId.length > 0) {
+    // Generate unique prefix
+    const hashFn = crypto.createHash("sha1");
+    hashFn.update(content);
+    const prefix = hashFn.digest("hex");
+
+    // Apply prefix to found nodes
+    nodesWithId.forEach(function (nodeWithId) {
+      const id = nodeWithId.getAttribute("id"),
+        newId = prefix + "-" + id;
+      nodeWithId.setAttribute("id", newId);
+
+      const attributesUsingId = xpath.select(
+        "//attribute::*[contains(., 'url(#" + id + ")')]",
+        targetDoc
+      );
+      attributesUsingId.forEach(function (attributeUsingId) {
+        attributeUsingId.value = "url(#" + newId + ")";
+      });
+    });
+  }
+
+  const markup = new XMLSerializer().serializeToString(targetDoc);
+  return "module.exports = " + JSON.stringify(markup);
+};
+
+module.exports.raw = true;
--- test/loader.spec.js
+++ test/loader.spec.js
@@ -1,228 +1,250 @@
-var fs = require('fs');
-var path = require('path');
-var assign = require('object-assign');
-var expect = require('expect.js');
-var webpack = require('webpack');
-var xmldom = require('xmldom');
-var xpath = require('xpath');
+const { describe, it, afterEach, expect } = require("@jest/globals");
+const fs = require("fs");
+const path = require("path");
+const webpack = require("webpack");
+const xmldom = require("xmldom");
+const xpath = require("xpath");
 
-describe('svg-as-symbol-loader', function() {
-    'use strict';
+describe("svg-as-symbol-loader", function () {
+  "use strict";
 
-    var outputDir = path.resolve(__dirname, './output'),
-        bundleFileName = 'bundle.js',
-        getBundleFile = function() {
-            return path.join(outputDir, bundleFileName);
-        };
-    var svgAsSymbolLoader = path.resolve(__dirname, '../');
-    var globalConfig = {
-        context: path.resolve(__dirname, '../'),
-		mode: 'development',
-        output: {
-            path: outputDir,
-            filename: bundleFileName
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.svg/,
-                    exclude: /node_modules/,
-                    loader: svgAsSymbolLoader,
-                    options: {}
-                }
-            ]
-        }
+  const outputDir = path.resolve(__dirname, "./output"),
+    bundleFileName = "bundle.js",
+    getBundleFile = function () {
+      return path.join(outputDir, bundleFileName);
     };
+  const svgAsSymbolLoader = path.resolve(__dirname, "../");
+  const globalConfig = {
+    context: path.resolve(__dirname, "../"),
+    mode: "development",
+    output: {
+      path: outputDir,
+      filename: bundleFileName,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.svg/,
+          exclude: /node_modules/,
+          loader: svgAsSymbolLoader,
+          options: {},
+        },
+      ],
+    },
+  };
 
-    // Clean generated cache files before each test so that we can call each test with an empty state.
-    afterEach(function(done) {
-        fs.unlink(getBundleFile(), done);
+  // Clean generated cache files before each test so that we can call each test with an empty state.
+  afterEach(function (done) {
+    fs.unlink(getBundleFile(), done);
+  });
+
+  it("should wrap SVG contents into symbol tag", function (done) {
+    const config = Object.assign({}, globalConfig, {
+      entry: "./test/input/icon.js",
     });
 
+    webpack(config, function (err, stats) {
+      expect(err).toBe(null);
+      fs.readFile(getBundleFile(), function (err, data) {
+        expect(err).toBe(null);
+        const encoded = eval(data.toString());
 
-    it('should wrap SVG contents into symbol tag', function(done) {
-        var config = assign({}, globalConfig, {
-            entry: './test/input/icon.js'
-        });
+        const outputDoc = new xmldom.DOMParser().parseFromString(
+          encoded,
+          "text/xml"
+        );
+        const outputEl = outputDoc.documentElement;
+        expect(outputEl.tagName).toBe("symbol");
 
-        webpack(config, function(err, stats) {
-            expect(err).to.be(null);
-            fs.readFile(getBundleFile(), function(err, data) {
-                expect(err).to.be(null);
-                var encoded = eval(data.toString());
+        const rectNodes = xpath.select("//rect", outputDoc);
+        expect(rectNodes).toHaveLength(1);
 
-				var outputDoc = new xmldom.DOMParser().parseFromString(encoded, 'text/xml');
-				var outputEl = outputDoc.documentElement;
-				expect(outputEl.tagName).to.be('symbol');
+        const circleNodes = xpath.select("//circle", outputDoc);
+        expect(circleNodes).toHaveLength(1);
 
-				var rectNodes = xpath.select("//rect", outputDoc);
-				expect(rectNodes.length).to.be(1);
+        return done();
+      });
+    });
+  });
 
-				var circleNodes = xpath.select("//circle", outputDoc);
-				expect(circleNodes.length).to.be(1);
-
-				return done();
-            });
-        });
+  it("should assign id attribute to generated symbol tag if it was provided by query", function (done) {
+    const config = Object.assign({}, globalConfig, {
+      entry: "./test/input/icon.js",
     });
 
-	it('should assign id attribute to generated symbol tag if it was provided by query', function(done) {
-		var config = assign({}, globalConfig, {
-			entry: './test/input/icon.js'
-		});
+    config.module.rules[0].options.id = "foo";
 
-		config.module.rules[0].options.id = 'foo';
+    webpack(config, function (err) {
+      expect(err).toBe(null);
+      fs.readFile(getBundleFile(), function (err, data) {
+        expect(err).toBe(null);
+        const encoded = eval(data.toString());
 
-		webpack(config, function(err) {
-			expect(err).to.be(null);
-			fs.readFile(getBundleFile(), function(err, data) {
-				expect(err).to.be(null);
-				var encoded = eval(data.toString());
+        const outputDoc = new xmldom.DOMParser().parseFromString(
+          encoded,
+          "text/xml"
+        );
+        const outputEl = outputDoc.documentElement;
+        expect(outputEl.getAttribute("id")).toBe("foo");
 
-				var outputDoc = new xmldom.DOMParser().parseFromString(encoded, 'text/xml');
-				var outputEl = outputDoc.documentElement;
-				expect(outputEl.getAttribute('id')).to.be('foo');
+        return done();
+      });
+    });
+  });
 
-				return done();
-			});
-		});
-	});
+  it("should assign class attribute to generated symbol tag if it was provided by query", function (done) {
+    const config = Object.assign({}, globalConfig, {
+      entry: "./test/input/icon.js",
+    });
 
-    it('should assign class attribute to generated symbol tag if it was provided by query', function(done) {
-		var config = assign({}, globalConfig, {
-			entry: './test/input/icon.js'
-		});
+    config.module.rules[0].options.class = "foo";
 
-		config.module.rules[0].options.class = 'foo';
+    webpack(config, function (err) {
+      expect(err).toBe(null);
+      fs.readFile(getBundleFile(), function (err, data) {
+        expect(err).toBe(null);
+        const encoded = eval(data.toString());
 
-		webpack(config, function(err) {
-			expect(err).to.be(null);
-			fs.readFile(getBundleFile(), function(err, data) {
-				expect(err).to.be(null);
-				var encoded = eval(data.toString());
+        const outputDoc = new xmldom.DOMParser().parseFromString(
+          encoded,
+          "text/xml"
+        );
+        const outputEl = outputDoc.documentElement;
+        expect(outputEl.getAttribute("class")).toBe("foo");
 
-				var outputDoc = new xmldom.DOMParser().parseFromString(encoded, 'text/xml');
-				var outputEl = outputDoc.documentElement;
-				expect(outputEl.getAttribute('class')).to.be('foo');
+        return done();
+      });
+    });
+  });
 
-				return done();
-			});
-		});
-	});
+  it("should use provided attribute instead of one found in SVG file if it is specified in query", function (done) {
+    const config = Object.assign({}, globalConfig, {
+      entry: "./test/input/icon.js",
+    });
 
-	it('should use provided attribute instead of one found in SVG file if it is specified in query', function(done) {
-		var config = assign({}, globalConfig, {
-			entry: './test/input/icon.js'
-		});
+    config.module.rules[0].options.height = "400px";
 
-		config.module.rules[0].options.height = '400px';
+    webpack(config, function (err) {
+      expect(err).toBe(null);
+      fs.readFile(getBundleFile(), function (err, data) {
+        expect(err).toBe(null);
+        const encoded = eval(data.toString());
 
-		webpack(config, function(err) {
-			expect(err).to.be(null);
-			fs.readFile(getBundleFile(), function(err, data) {
-				expect(err).to.be(null);
-				var encoded = eval(data.toString());
+        const outputDoc = new xmldom.DOMParser().parseFromString(
+          encoded,
+          "text/xml"
+        );
+        const outputEl = outputDoc.documentElement;
+        expect(outputEl.getAttribute("height")).toBe("400px");
 
-				var outputDoc = new xmldom.DOMParser().parseFromString(encoded, 'text/xml');
-				var outputEl = outputDoc.documentElement;
-				expect(outputEl.getAttribute('height')).to.be('400px');
+        return done();
+      });
+    });
+  });
 
-				return done();
-			});
-		});
-	});
+  it("should use given tag instead of default symbol tag if it was provided by query", function (done) {
+    const config = Object.assign({}, globalConfig, {
+      entry: "./test/input/icon.js",
+    });
 
-	it('should use given tag instead of default symbol tag if it was provided by query', function(done) {
-		var config = assign({}, globalConfig, {
-			entry: './test/input/icon.js'
-		});
+    config.module.rules[0].options.tag = "pattern";
 
-		config.module.rules[0].options.tag = 'pattern';
+    webpack(config, function (err) {
+      expect(err).toBe(null);
+      fs.readFile(getBundleFile(), function (err, data) {
+        expect(err).toBe(null);
+        const encoded = eval(data.toString());
 
-		webpack(config, function(err) {
-			expect(err).to.be(null);
-			fs.readFile(getBundleFile(), function(err, data) {
-				expect(err).to.be(null);
-				var encoded = eval(data.toString());
+        const outputDoc = new xmldom.DOMParser().parseFromString(
+          encoded,
+          "text/xml"
+        );
+        const outputEl = outputDoc.documentElement;
+        expect(outputEl.tagName).toBe("pattern");
 
-				var outputDoc = new xmldom.DOMParser().parseFromString(encoded, 'text/xml');
-				var outputEl = outputDoc.documentElement;
-				expect(outputEl.tagName).to.be('pattern');
+        return done();
+      });
+    });
+  });
 
-				return done();
-			});
-		});
-	});
+  it("should be able to use SVG tag instead of default symbol tag if it was provided by query", function (done) {
+    const config = Object.assign({}, globalConfig, {
+      entry: "./test/input/icon.js",
+    });
 
-	it('should be able to use SVG tag instead of default symbol tag if it was provided by query', function(done) {
-		var config = assign({}, globalConfig, {
-			entry: './test/input/icon.js'
-		});
+    config.module.rules[0].options.tag = "svg";
 
-		config.module.rules[0].options.tag = 'svg';
+    webpack(config, function (err) {
+      expect(err).toBe(null);
+      fs.readFile(getBundleFile(), function (err, data) {
+        expect(err).toBe(null);
+        const encoded = eval(data.toString());
 
-		webpack(config, function(err) {
-			expect(err).to.be(null);
-			fs.readFile(getBundleFile(), function(err, data) {
-				expect(err).to.be(null);
-				var encoded = eval(data.toString());
+        const outputDoc = new xmldom.DOMParser().parseFromString(
+          encoded,
+          "text/xml"
+        );
+        const outputEl = outputDoc.documentElement;
+        expect(outputEl.tagName).toBe("svg");
 
-				var outputDoc = new xmldom.DOMParser().parseFromString(encoded, 'text/xml');
-				var outputEl = outputDoc.documentElement;
-				expect(outputEl.tagName).to.be('svg');
+        return done();
+      });
+    });
+  });
 
-				return done();
-			});
-		});
-	});
+  it("should append random prefix to all ID attributes used", function (done) {
+    const config = Object.assign({}, globalConfig, {
+      entry: "./test/input/icon.js",
+    });
 
-	it('should append random prefix to all ID attributes used', function(done) {
-		var config = assign({}, globalConfig, {
-			entry: './test/input/icon.js'
-		});
+    webpack(config, function (err) {
+      expect(err).toBe(null);
+      fs.readFile(getBundleFile(), function (err, data) {
+        expect(err).toBe(null);
+        const encoded = eval(data.toString());
 
-		webpack(config, function(err) {
-			expect(err).to.be(null);
-			fs.readFile(getBundleFile(), function(err, data) {
-				expect(err).to.be(null);
-				var encoded = eval(data.toString());
+        const outputDoc = new xmldom.DOMParser().parseFromString(
+          encoded,
+          "text/xml"
+        );
 
-				var outputDoc = new xmldom.DOMParser().parseFromString(encoded, 'text/xml');
+        const linearGradient = xpath.select("//linearGradient", outputDoc);
+        expect(linearGradient).toHaveLength(1);
+        const prefixedId = linearGradient[0].getAttribute("id");
 
-				var linearGradient = xpath.select("//linearGradient", outputDoc);
-				expect(linearGradient.length).to.be(1);
-				var prefixedId = linearGradient[0].getAttribute('id');
+        const path = xpath.select("//path", outputDoc);
+        expect(path).toHaveLength(1);
+        expect(path[0].getAttribute("fill")).toBe("url(#" + prefixedId + ")");
 
-				var path = xpath.select("//path", outputDoc);
-				expect(path.length).to.be(1);
-				expect(path[0].getAttribute('fill')).to.be('url(#' + prefixedId + ')');
+        return done();
+      });
+    });
+  });
 
-				return done();
-			});
-		});
-	});
+  it("should interpolate ID and class values", function (done) {
+    const config = Object.assign({}, globalConfig, {
+      entry: "./test/input/icon.js",
+    });
 
-    it('should interpolate ID and class values', function(done) {
-        var config = assign({}, globalConfig, {
-			entry: './test/input/icon.js'
-		});
+    config.module.rules[0].options.class = "[name].[ext]";
+    config.module.rules[0].options.id = "[name]";
 
-		config.module.rules[0].options.class = '[name].[ext]';
-		config.module.rules[0].options.id = '[name]';
+    webpack(config, function (err) {
+      expect(err).toBe(null);
+      fs.readFile(getBundleFile(), function (err, data) {
+        expect(err).toBe(null);
+        const encoded = eval(data.toString());
 
-		webpack(config, function(err) {
-			expect(err).to.be(null);
-			fs.readFile(getBundleFile(), function(err, data) {
-				expect(err).to.be(null);
-				var encoded = eval(data.toString());
+        const outputDoc = new xmldom.DOMParser().parseFromString(
+          encoded,
+          "text/xml"
+        );
+        const outputEl = outputDoc.documentElement;
+        expect(outputEl.getAttribute("class")).toBe("icon.svg");
+        expect(outputEl.getAttribute("id")).toBe("icon");
 
-				var outputDoc = new xmldom.DOMParser().parseFromString(encoded, 'text/xml');
-				var outputEl = outputDoc.documentElement;
-				expect(outputEl.getAttribute('class')).to.be('icon.svg');
-				expect(outputEl.getAttribute('id')).to.be('icon');
-
-				return done();
-			});
-		});
-	});
+        return done();
+      });
+    });
+  });
 });
